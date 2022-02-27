3eb34e269f632be328e79fb2c9374834d46ebef5
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-02-27 14:37:15 +0100

    Mockito UserService first test

--- src/test/java/com/sda/testing/solution/mockito/user/UserServiceTest.java
+++ src/test/java/com/sda/testing/solution/mockito/user/UserServiceTest.java
@@ -1,15 +1,22 @@
 package com.sda.testing.solution.mockito.user;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
-	@Mock
+	@Mock(lenient = true)
 	private UserRepository userRepository;
 
 	@Mock
@@ -18,9 +25,20 @@ class UserServiceTest {
 	@InjectMocks
 	private UserService userService;
 
+
 	@Test
 	void shouldReturnProperUser() {
+		User savedUser = new User(4L, "Tomek", "Woźniak");
 
+		when(userRepository.findById(savedUser.getId())).thenReturn(Optional.of(savedUser));
+
+		User user = userService.getUserById(4L);
+
+	/*	assertNotNull(user);
+		assertEquals("Tomek", user.getFirstName());
+		assertEquals("Woźniak", user.getLastName());
+	*/
+		assertEquals(savedUser, user);
 	}
 
 	@Test

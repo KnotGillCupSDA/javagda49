9826d626375c50758d698c4d9de0227387e550a7
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-02-27 09:57:07 +0100

    -- first test

--- src/test/java/com/sda/testing/solution/parametrized/DivisibilityCheckerFirstTest.java
+++ src/test/java/com/sda/testing/solution/parametrized/DivisibilityCheckerFirstTest.java
@@ -1,5 +1,6 @@
 package com.sda.testing.solution.parametrized;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ class DivisibilityCheckerFirstTest {
 	@Test
 	void shouldBeDivisibleBy3() {
 		//given
-		int i = 9;
+		int i = 12;
 
 		//when
 		boolean actual = DivisibilityChecker.isDivisibleBy3(i);
@@ -20,17 +21,49 @@ class DivisibilityCheckerFirstTest {
 
 	@Test
 	void shouldNotBeDivisibleBy3() {
+		//given
+		int i = 11;
+
+		//when
+		boolean actual = DivisibilityChecker.isDivisibleBy3(i);
+
+		//then
+		assertFalse(actual);
 	}
 
 	@Test
 	void shouldBeDivisibleBy3NegativeNumber() {
+		//given
+		int i = -12;
+
+		//when
+		boolean actual = DivisibilityChecker.isDivisibleBy3(i);
+
+		//then
+		assertTrue(actual);
 	}
 
 	@Test
 	void shouldNotBeDivisibleBy3NegativeNumber() {
+		//given
+		int i = -11;
+
+		//when
+		boolean actual = DivisibilityChecker.isDivisibleBy3(i);
+
+		//then
+		assertFalse(actual);
 	}
 
 	@Test
 	void zeroShouldBeDivisibleBy3() {
+		//given
+		int i = 0;
+
+		//when
+		boolean actual = DivisibilityChecker.isDivisibleBy3(i);
+
+		//then
+		assertTrue(actual);
 	}
 }

44eeb323a4fb50b16227bc60495e2fb993315a86
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-02-27 10:05:08 +0100

    -- DivisibilityCheckerTest draft

---
+++ src/test/java/com/sda/testing/solution/parametrized/DivisibilityCheckerTest.java
@@ -0,0 +1,20 @@
+package com.sda.testing.solution.parametrized;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class DivisibilityCheckerTest {
+
+	@ParameterizedTest
+	@ValueSource(ints = {0, 3, 9, 12, 15, -3, -6})
+	void shouldBeDivisibleBy3(int number) {
+		//when
+		boolean actual = DivisibilityChecker.isDivisibleBy3(number);
+
+		//then
+		assertTrue(actual);
+	}
+
+}

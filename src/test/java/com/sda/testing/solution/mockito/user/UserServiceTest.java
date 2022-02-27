1f5887412dedbb95bea5ba0dfe86a937c94beae0
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-02-27 15:05:45 +0100

    Mockito UserService exception test

--- src/test/java/com/sda/testing/solution/mockito/user/UserServiceTest.java
+++ src/test/java/com/sda/testing/solution/mockito/user/UserServiceTest.java
@@ -1,22 +1,21 @@
 package com.sda.testing.solution.mockito.user;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 import java.util.Optional;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
-	@Mock(lenient = true)
+	@Mock
 	private UserRepository userRepository;
 
 	@Mock
@@ -29,20 +28,17 @@ class UserServiceTest {
 	@Test
 	void shouldReturnProperUser() {
 		User savedUser = new User(4L, "Tomek", "Woźniak");
-
 		when(userRepository.findById(savedUser.getId())).thenReturn(Optional.of(savedUser));
 
 		User user = userService.getUserById(4L);
 
-	/*	assertNotNull(user);
-		assertEquals("Tomek", user.getFirstName());
-		assertEquals("Woźniak", user.getLastName());
-	*/
 		assertEquals(savedUser, user);
 	}
 
 	@Test
 	void shouldThrowExceptionWhenUserNotExists() {
-
+		Long notExistingId = 878L;
+		when(userRepository.findById(notExistingId)).thenReturn(Optional.empty());
+		assertThrows(Exception.class, () -> userService.getUserById(notExistingId));
 	}
 }

d601cb79c9badcbfeae6b643460f2ae48fc074f7
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-02-27 11:33:08 +0100

    -- ConversionType done

--- src/test/java/com/sda/testing/solution/conversion/MeasurementConverterTest.java
+++ src/test/java/com/sda/testing/solution/conversion/MeasurementConverterTest.java
@@ -1,12 +1,35 @@
 package com.sda.testing.solution.conversion;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class MeasurementConverterTest {
 
-	@Test
-	void name() {
+	@ParameterizedTest
+	@EnumSource(value = ConversionType.class, names = {"YARDS_TO_METERS", "METERS_TO_YARDS"})
+	void test(ConversionType conversionType) {
+	}
+
+	@ParameterizedTest
+	@EnumSource
+	void shouldAlwaysConvertToDoubleWithinBoundaries(ConversionType conversionType) {
+		double convert = new MeasurementConverter().convert(1032342, conversionType);
+
+		assertTrue(convert < Double.MAX_VALUE);
+		assertTrue(convert > Double.MIN_VALUE);
+		assertNotEquals(Double.NaN, convert);
+	}
+
+	@ParameterizedTest
+	@EnumSource(names = {"METERS_TO_YARDS", "INCHES_TO_CENTIMETERS", "MILES_TO_KILOMETERS"})
+	public void shouldConvertToHigherValue(ConversionType conversionType) {
+		int i = 100;
+
+		double converted = new MeasurementConverter().convert(i, conversionType);
+
+		assertTrue(converted > i);
 	}
 }

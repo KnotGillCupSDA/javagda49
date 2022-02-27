db5458579e65c419e23f98fc55ad5229f5ca6907
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-02-27 12:39:55 +0100

    -- RomanNumeralConverterTest with parametrization

--- src/test/java/com/sda/testing/solution/parametrized/romannumeral/RomanNumeralConverterTest.java
+++ src/test/java/com/sda/testing/solution/parametrized/romannumeral/RomanNumeralConverterTest.java
@@ -2,16 +2,23 @@ package com.sda.testing.solution.parametrized.romannumeral;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
 
 class RomanNumeralConverterTest {
 
-	@Test
-	void shouldUseRomanValuesFromEnum() {
-		String roman = RomanNumeralConverter.romanFor(
-				RomanNumeralConverter.ArabicToRoman.ONE.getArabic());
+	@ParameterizedTest
+	@EnumSource
+	void shouldUseRomanValuesFromEnum(RomanNumeralConverter.ArabicToRoman arabicToRoman) {
+		String roman = RomanNumeralConverter.romanFor(arabicToRoman.getArabic());
+		assertEquals(arabicToRoman.getRoman(), roman);
+	}
 
-		assertEquals(RomanNumeralConverter.ArabicToRoman.ONE.getRoman(),
-				roman);
+	@ParameterizedTest
+	@CsvSource({"4999, MMMMCMXCIX", "3462, MMMCDLXII","538, DXXXVIII" })
+	public void dummy(int arabic, String expected) {
+		String roman = RomanNumeralConverter.romanFor(arabic);
+		assertEquals(expected, roman);
 	}
 }

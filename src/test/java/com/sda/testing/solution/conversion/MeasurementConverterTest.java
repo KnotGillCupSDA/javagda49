2471b85a43c05a8d4edbe0defccf7010647a98e3
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-02-27 13:31:12 +0100

    MeasurementConverterTest with MethodSource done

--- src/test/java/com/sda/testing/solution/conversion/MeasurementConverterTest.java
+++ src/test/java/com/sda/testing/solution/conversion/MeasurementConverterTest.java
@@ -4,22 +4,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class MeasurementConverterTest {
 
+	private MeasurementConverter measurementConverter;
+
+	@BeforeEach
+	void setUp() {
+		measurementConverter = new MeasurementConverter();
+	}
+
 	@ParameterizedTest
-	@EnumSource(value = ConversionType.class, names = {"YARDS_TO_METERS", "METERS_TO_YARDS"})
+	@EnumSource(value = ConversionType.class, names = { "YARDS_TO_METERS", "METERS_TO_YARDS" })
 	void test(ConversionType conversionType) {
 	}
 
 	@ParameterizedTest
 	@EnumSource
 	void shouldAlwaysConvertToDoubleWithinBoundaries(ConversionType conversionType) {
-		double convert = new MeasurementConverter().convert(1032342, conversionType);
+		double convert = measurementConverter.convert(1032342, conversionType);
 
 		assertTrue(convert < Double.MAX_VALUE);
 		assertTrue(convert > Double.MIN_VALUE);
@@ -27,23 +37,29 @@ class MeasurementConverterTest {
 	}
 
 	@ParameterizedTest
-	@EnumSource(names = {"METERS_TO_YARDS", "INCHES_TO_CENTIMETERS", "MILES_TO_KILOMETERS"})
+	@EnumSource(names = { "METERS_TO_YARDS", "INCHES_TO_CENTIMETERS", "MILES_TO_KILOMETERS" })
 	public void shouldConvertToHigherValue(ConversionType conversionType) {
 		int i = 100;
-		double converted = new MeasurementConverter().convert(i, conversionType);
+		double converted = measurementConverter.convert(i, conversionType);
 		assertTrue(converted > i);
 	}
-
 
 	@ParameterizedTest
 	@MethodSource
 	void someOperationsShouldBeReversible(ConversionType c1, ConversionType c2) {
-		int initialValue = 4234;
+		double initialValue = 12345;
+		assertEquals(initialValue,
+				measurementConverter.convert(measurementConverter.convert(initialValue, c1), c2));
+	}
 
-		MeasurementConverter measurementConverter = new MeasurementConverter();
-		double converted1 = measurementConverter.convert(initialValue, c1);
-		double converted2 = measurementConverter.convert(converted1, c2);
-
-		assertEquals(initialValue, converted2);
+	public static Stream<Arguments> someOperationsShouldBeReversible() {
+		return Stream.of(
+				Arguments.of(ConversionType.METERS_TO_YARDS, ConversionType.YARDS_TO_METERS),
+				Arguments.of(ConversionType.YARDS_TO_METERS, ConversionType.METERS_TO_YARDS),
+				Arguments.of(ConversionType.CENTIMETERS_TO_INCHES, ConversionType.INCHES_TO_CENTIMETERS),
+				Arguments.of(ConversionType.INCHES_TO_CENTIMETERS, ConversionType.CENTIMETERS_TO_INCHES),
+				Arguments.of(ConversionType.KILOMETERS_TO_MILES, ConversionType.MILES_TO_KILOMETERS),
+				Arguments.of(ConversionType.MILES_TO_KILOMETERS, ConversionType.KILOMETERS_TO_MILES)
+		);
 	}
 }

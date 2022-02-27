ece9cf7a226cb39b50ad9716a92069a130c20e9b
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-02-27 11:14:29 +0100

    -- ConversionType example

---
+++ src/test/java/com/sda/testing/solution/conversion/MeasurementConverterTest.java
@@ -0,0 +1,12 @@
+package com.sda.testing.solution.conversion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MeasurementConverterTest {
+
+	@Test
+	void name() {
+	}
+}
